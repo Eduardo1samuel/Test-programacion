763984636515a45ab62ab3ea9f1e10f0da22c8f4
Eduardo1samuel <samuelduguol56@gmail.com>
2024-07-26 10:07:50 -0600

    segundo commit

--- SwitchCase.py
+++ SwitchCase.py
@@ -1,23 +1,31 @@
-class SentenciaSwitch:
-    
-    def switch(self,mes):
-        default = "Mes incorrecto"
-        return getattr(self,'case_' + str(mes),lambda:default)()
-    
-    def case_1(self):
-        return "Enero"
-    
-    def case_2(self):
-        return "Febrero"
-    
-    def case_3(self):
-        return "Marzo"
-    
-    def case_4(self):
-        return "Abril"
-    
-    def case_5(self):
-        return "Mayo"
-    
-    def case_6(self):
-        return "Junio"
+
+def menu(opcion):
+    opciones = {
+        1: "Opción 1: Ver información",
+        2: "Opción 2: Realizar proceso de registro",
+        3: "Opción 3: Salir"
+    }
+    return opciones.get(opcion, "Opción no válida")
+
+
+print("Seleccione una opción:")
+print("1. Ver información")
+print("2. Realizar proceso de registro")
+print("3. Salir")
+
+try:
+    opcion_usuario = int(input("Ingresa tu opción: "))
+    resultado = menu(opcion_usuario)
+    print(resultado)
+
+    if opcion_usuario == 1:
+        print("Incripciones abiertas desde el 5 de enero")
+    elif opcion_usuario == 2:
+        print("Registrarse")
+    elif opcion_usuario == 3:
+        print("Saliendo del programa...")
+    else:
+        print("Por favor, elige una opción válida.")
+
+except ValueError:
+    print("Por favor, ingresa un número válido.")
